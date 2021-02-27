407c69d457407078e55abddd116916254fb5df34
Karel Novak <carlosusa@seznam.cz>
2021-02-27 18:36:40 +0100

    csv output

--- elect.py
+++ elect.py
@@ -5,61 +5,55 @@ import csv
 import sys
 import os
 import time
+
 response = requests.get('https://volby.cz/')
 print(response.status_code)
 
-header = ['kód obce', 'název obce', 'voliči v seznamu', 'vydané obálky', 'platné hlasy',
+HEADER = ['kód obce', 'název obce', 'voliči v seznamu', 'vydané obálky', 'platné hlasy',
           'ODS', 'Řád národa', 'Cesta OS', 'ČSSD', 'Pravý blok', 'Radostné Česko',
           'STAN', 'KSČM', 'Strana zelených', 'ROZUMNÍ', 'Spol.Prokop.údolí', 'SSO',
           'Blok proti islámu', 'ODA', 'Piráti', 'Občané 2011', 'Unie H.A.V.E.L', 'ČNF',
           'Referendum o EU', 'TOP 09', 'ANO 2011', 'Dobrá volba 2016', 'SPR-RSČ', 'KDU-ČSL',
           'ČSSS', 'REALISTÉ', 'SPORTOVCI', 'DSSS', 'SPD', 'SPO', 'Národ sobě']
 
-list_of_lists = list()
-
-
 
 def main():
 
-    print(FIRST_PATH)
     main_tuple = get_links(FIRST_PATH)
     list_of_lists = get_list_of_values(main_tuple)
-    election_file(FILE, header, list_of_lists)
+    election_file(FILE, HEADER, list_of_lists)
 
 
-def election_file(FILE: str, header: list, list_of_lists: list) -> csv:
-    with open(FILE, 'w') as f:
+def election_file(e_file: str, e_header: list, e_list_of_lists: list) -> csv:
+    with open(e_file, 'w') as f:
         f_writer = csv.writer(f)
-        f_writer.writerow(header)
-        f_writer.writerows(list_of_lists)
+        f_writer.writerow(e_header)
+        f_writer.writerows(e_list_of_lists)
 
 
-
-def get_list_of_values(main_tuple: tuple) -> list:
+def get_list_of_values(v_tuple: tuple) -> list:
     list_of_values = list()
-    for i in range(len(main_tuple[0])):
-        path = main_tuple[1][i]
-        x1 = main_tuple[0][i]
+    for i in range(len(v_tuple[0])):
+        path = v_tuple[1][i]
+        x1 = v_tuple[0][i]
         d = get_numbers(x1, path)
         list_of_values.append(d)
     return list_of_values
 
 
-def get_links(first_path: str) -> tuple:
+def get_links(l_path: str) -> tuple:
     list_links = list()
     list_numbers = list()
     c1 = list()
-    getr = requests.get(first_path)
+    getr = requests.get(l_path)
     soup = bs4.BeautifulSoup(getr.text, 'html.parser')
     c = (soup.find_all('a')[5:-2:2])
     for item in c:
-        item = str(item)
-        x = item.replace('amp;', '')
-        c1.append(x)
+        c1.append(str(item).replace('amp;', ''))
     for i in range(len(c1)):
-        x1 = format(c1[i]).split(('"'))[1].split('=')[3].split('&')[0]
+        x1 = format(c1[i].split('"')[1].split('=')[3].split('&')[0])
         list_numbers.append(int(x1))
-        path = os.path.join('https://www.volby.cz/pls/ps2017nss/', format(c1[i]).split(('"'))[1])
+        path = os.path.join('https://www.volby.cz/pls/ps2017nss/', format(c1[i]).split('"')[1])
         list_links.append(path)
     return list_numbers, list_links
 
@@ -72,7 +66,7 @@ def get_numbers(x1: int, path: str) -> list:
     c = soup.find_all('h3')
     x2 = [(str(c[2]).split()[2])]
 
-    x = dict(pd.read_html(path, encoding= 'UTF-8')[0])
+    x = dict(pd.read_html(path, encoding='UTF-8')[0])
     x3 = list(x[('Voličiv seznamu', 'Voličiv seznamu')].values)
     x4 = list(x[('Vydanéobálky', 'Vydanéobálky')].values)
     x5 = list(x[('Platnéhlasy', 'Platnéhlasy')].values)
@@ -140,9 +134,11 @@ def num_conversion(my_list: list) -> list:
 
 
 if __name__ == '__main__':
-    FIRST_PATH = sys.argv[1]  # have to enclose it in double quotes!!!!!!
+    FIRST_PATH = sys.argv[1]  # have to enclose it in double quotes!!!
     FILE = sys.argv[2]
     start = time.time()
     main()
     end = time.time()
-    print('trvalo to: ', end - start)
+    print('it took: ', end - start)
+
+print()
